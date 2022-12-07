db9b3acec4693b1932e90a7243769e3aa4c16421
cwesterhoff01 <93213402+cwesterhoff01@users.noreply.github.com>
2022-12-06 21:22:23 -0700

    update

--- WebPage/public/javascripts/display.js
+++ WebPage/public/javascripts/display.js
@@ -19,3 +19,33 @@ function logout() {
     localStorage.removeItem("token");
     window.location.replace("index.html");
 }
+$(function () {
+    $('#fetchData').click(fetchDevice);
+    
+});
+//Graphing stuff i guess
+function fetchData() {
+    $.ajax({
+        url: '/devices/device',
+        method: 'GET',
+        contentType: 'application/json',
+        headers: {'x-auth': localStorage.getItem("token")},
+        dataType: 'json'
+    }).done(function(data, textStats, jqXHR) {
+        var myData = JSON.parse(data.param);
+        var lastDevice = myData.length - 1;
+        var lastData = myData[lastDevice].data.length - 1;
+
+        
+        if(lastData >= 1){
+            console.log(myData[lastDevice].data[lastData]);
+        }
+        else{
+            console.log("No data found");
+        }
+        //window.alert(`Success: ${data.responseJSON['msg']}`);
+    }).fail(function(data, textStatus, jqXHR) {
+        //window.alert(`Error: ${data.responseJSON['msg']}`);
+        console.log("fail");
+    });
+}
